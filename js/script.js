03e8ef126423c3715ade788680bac0a56808edf4
Sammynauta <samuka.o.diogo@gmail.com>
2021-10-05 15:08:25 -0300

    home e about inseridos concluir style deles.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
-const btnMenu = document.getElementById('menu'),
+const btnMenu = document.getElementById('menu_icon'),
 menuHeader = document.querySelector(".menu_header"),
-navlink = document.querySelectorAll(".nav_link");
-
+navlink = document.querySelectorAll(".nav_link"),
+closeMenu = document.getElementById("close_icon");
 
 //MOSTRAR MENU
 function toggleMenu (){
     menuHeader.classList.toggle("active");
+    closeMenu.classList.toggle("active");
+    btnMenu.classList.add('none');
 }
 
+//FECHAR MENU ALTERANDO ICON
+function removeMenu () {
+    btnMenu.classList.remove('none');
+    closeMenu.classList.remove("active");
+    menuHeader.classList.remove("active");
+}
+
+closeMenu.addEventListener('click', removeMenu);
 btnMenu.addEventListener('click', toggleMenu);
 
 //REMOVE MENU CLICANDO NAV LINK DO MENU
 function linkaction (){
     menuHeader.classList.remove('active');
+    btnMenu.classList.remove('none');
+    closeMenu.classList.remove("active");
 }
 navlink.forEach(n => n.addEventListener('click', linkaction));
-
